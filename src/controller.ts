1cc0f0d8fc17186563cdc8c54d7497d788c09611
Jae Won Jang <jjang16@illinois.edu>
2023-06-16 00:47:51 +0900

    registers SafeAreaElement in browser only in order to prevent breaking evaluating SafeAreaController in SSR environments.

--- src/controller.ts
+++ src/controller.ts
@@ -3,67 +3,70 @@ import { Capacitor } from '@capacitor/core';
 import type { SafeAreaType } from './index';
 import { SafeArea } from './index';
 
-class SafeAreaElement extends HTMLElement {
-  mode: 'padding' | 'margin';
-  edges?: string;
+if (typeof window !== `undefined`) {
+  class SafeAreaElement extends HTMLElement {
+    mode: 'padding' | 'margin';
+    edges?: string;
 
-  constructor() {
-    super();
+    constructor() {
+      super();
 
-    this.mode = 'padding';
-    // Create a shadow root
-    this.attachShadow({ mode: 'open' });
-  }
-
-  connectedCallback() {
-    this.renderElement();
-  }
-
-  static get observedAttributes() {
-    return ['mode', 'edges'];
-  }
-
-  attributeChangedCallback(name: string, oldValue: string, newValue: string) {
-    if (oldValue === newValue) {
-      return;
+      this.mode = 'padding';
+      // Create a shadow root
+      this.attachShadow({ mode: 'open' });
     }
-    (this as any)[name] = newValue;
-    this.renderElement();
-  }
 
-  async renderElement() {
-    if (this.shadowRoot) {
-      const safeAreaInset = await SafeArea.getSafeAreaInsets();
+    connectedCallback() {
+      this.renderElement();
+    }
 
-      const isMargin = this.mode === 'margin';
-      const edges = this.edges?.split(',').map(item => item.trim());
+    static get observedAttributes() {
+      return ['mode', 'edges'];
+    }
 
-      const insets = {
-        top: edges && !edges?.includes('top') ? 0 : safeAreaInset.top,
-        bottom: edges && !edges?.includes('bottom') ? 0 : safeAreaInset.bottom,
-        left: edges && !edges?.includes('left') ? 0 : safeAreaInset.left,
-        right: edges && !edges?.includes('right') ? 0 : safeAreaInset.right,
-      };
+    attributeChangedCallback(name: string, oldValue: string, newValue: string) {
+      if (oldValue === newValue) {
+        return;
+      }
+      (this as any)[name] = newValue;
+      this.renderElement();
+    }
 
-      this.shadowRoot.innerHTML = `
-        <style>
-          #wrapper {
-            ${isMargin ? 'margin' : 'padding'}-top: ${insets.top}px;
-            ${isMargin ? 'margin' : 'padding'}-bottom: ${insets.bottom}px;
-            ${isMargin ? 'margin' : 'padding'}-left: ${insets.left}px;
-            ${isMargin ? 'margin' : 'padding'}-right: ${insets.right}px;
-          }
-        </style>
-        <div id="wrapper">
-          <slot></slot>
-        </div>
-      `;
+    async renderElement() {
+      if (this.shadowRoot) {
+        const safeAreaInset = await SafeArea.getSafeAreaInsets();
+
+        const isMargin = this.mode === 'margin';
+        const edges = this.edges?.split(',').map(item => item.trim());
+
+        const insets = {
+          top: edges && !edges?.includes('top') ? 0 : safeAreaInset.top,
+          bottom: edges && !edges?.includes('bottom') ? 0 : safeAreaInset.bottom,
+          left: edges && !edges?.includes('left') ? 0 : safeAreaInset.left,
+          right: edges && !edges?.includes('right') ? 0 : safeAreaInset.right,
+        };
+
+        this.shadowRoot.innerHTML = `
+          <style>
+            #wrapper {
+              ${isMargin ? 'margin' : 'padding'}-top: ${insets.top}px;
+              ${isMargin ? 'margin' : 'padding'}-bottom: ${insets.bottom}px;
+              ${isMargin ? 'margin' : 'padding'}-left: ${insets.left}px;
+              ${isMargin ? 'margin' : 'padding'}-right: ${insets.right}px;
+            }
+          </style>
+          <div id="wrapper">
+            <slot></slot>
+          </div>
+        `;
+      }
     }
   }
+
+  if (!customElements.get('safe-area'))
+    customElements.define('safe-area', SafeAreaElement);
+
 }
-
-if (!customElements.get('safe-area'))
-  customElements.define('safe-area', SafeAreaElement);
 
 class SafeAreaController {
   async injectCSSVariables(): Promise<void> {
