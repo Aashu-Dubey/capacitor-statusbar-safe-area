0e4a144c434bf55a3eaf3846bf401cfab38b9eeb
Jae Won Jang <jjang16@illinois.edu>
2023-06-16 10:26:11 +0900

    separated safe area element from safe area controller, made safe area element registration opt-in

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,7 @@ import { registerPlugin } from '@capacitor/core';
 
 import SafeAreaController from './controller';
 import type { SafeAreaPlugin, SafeAreaHTMLProps } from './definitions';
+import { SafeAreaElement, registerSafeAreaElement } from './element';
 
 const SafeArea = registerPlugin<SafeAreaPlugin>('SafeArea', {
   web: () => import('./web').then(m => new m.SafeAreaWeb()),
@@ -12,6 +13,7 @@ const controller = new SafeAreaController();
 export * from './definitions';
 export { SafeArea };
 export { controller as SafeAreaController };
+export { SafeAreaElement, registerSafeAreaElement };
 
 declare global {
   // eslint-disable-next-line @typescript-eslint/no-namespace
