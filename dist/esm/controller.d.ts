29dc40b8e4902917fa6f6186061a46cd84601675
Tom Callahan <tom@tom-callahan.com>
2025-02-21 19:44:49 -0500

    remove temp dist addition

--- dist/esm/controller.d.ts
+++
@@ -1,5 +0,0 @@
-export declare class SafeAreaController {
-    injectCSSVariables(): Promise<void>;
-    addStatusBarHeight(): Promise<void>;
-    addSafeAreaVariables(): Promise<void>;
-}
--- dist/esm/web.d.ts
+++
@@ -1,8 +0,0 @@
-import { WebPlugin } from '@capacitor/core';
-import type { SafeAreaPlugin, SafeAreaInset } from './definitions';
-export declare class SafeAreaWeb extends WebPlugin implements SafeAreaPlugin {
-    getStatusBarHeight(): Promise<{
-        height: number;
-    }>;
-    getSafeAreaInsets(): Promise<SafeAreaInset>;
-}
