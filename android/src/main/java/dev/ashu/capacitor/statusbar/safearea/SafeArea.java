817461e3e385a3a962a5cf2866d13d6bdfe5603a
Ashu <ashud.contact@gmail.com>
2026-08-19 18:26:56 +0530

    fix - refactor: updated implementations to use latest APIs and approach:
    => iOS: removed the deprecated `UIApplication.shared.windows` usage with recommended `UIWindowScene.windows`, and a common function to access the reference
    
    => Android
    1. status bar height: use recommended `WindowInsetsCompat.getInsets()` to get the height and kept `res.getIdentifier()` as a fallback.
    Also instead of returning `null` in case of no height value,  it'll return an object with `height: 0`
    
    2. safe area insets: removed marshmellow condition as minimum target it now 24, and migrated `WindowInsets` to use `WindowInsetsCompat` for backward compatibility and removing the API version condtional logic

--- android/src/main/java/dev/ashu/capacitor/statusbar/safearea/SafeArea.java
+++ android/src/main/java/dev/ashu/capacitor/statusbar/safearea/SafeArea.java
@@ -1,11 +1,13 @@
 package dev.ashu.capacitor.statusbar.safearea;
 
+import android.annotation.SuppressLint;
 import android.app.Activity;
 import android.content.res.Resources;
-import android.graphics.Insets;
 import android.os.Build;
 import android.view.DisplayCutout;
 import android.view.WindowInsets;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 import com.getcapacitor.JSObject;
 
@@ -13,16 +15,25 @@ public class SafeArea {
 
     public JSObject getStatusBarHeight(Activity activity) {
         Resources res = activity.getApplicationContext().getResources();
-        int statusBarHeight;
-        int resourceId = res.getIdentifier("status_bar_height", "dimen", "android");
-        if (resourceId > 0) {
-            statusBarHeight = res.getDimensionPixelSize(resourceId);
-            JSObject ret = new JSObject();
-            ret.put("height", statusBarHeight / res.getDisplayMetrics().density);
-            return ret;
-        } else {
-            return null;
+        float density = res.getDisplayMetrics().density;
+        int statusBarHeightPx = 0;
+
+        WindowInsetsCompat insets = ViewCompat.getRootWindowInsets(activity.getWindow().getDecorView());
+        if (insets != null) {
+            statusBarHeightPx = insets.getInsets(WindowInsetsCompat.Type.statusBars()).top;
         }
+
+        if (statusBarHeightPx == 0) {
+            @SuppressLint({ "InternalInsetResource", "DiscouragedApi" })
+            int resourceId = res.getIdentifier("status_bar_height", "dimen", "android");
+            if (resourceId > 0) {
+                statusBarHeightPx = res.getDimensionPixelSize(resourceId);
+            }
+        }
+
+        JSObject ret = new JSObject();
+        ret.put("height", statusBarHeightPx / density);
+        return ret;
     }
 
     public JSObject getSafeAreaInsets(Activity activity) {
@@ -30,41 +41,16 @@ public class SafeArea {
             rightInset = 0,
             topInset = 0,
             bottomInset = 0;
-        if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.M) {
-            Resources res = activity.getApplicationContext().getResources();
-            WindowInsets windowInsets = activity.getWindow().getDecorView().getRootWindowInsets();
+        Resources res = activity.getApplicationContext().getResources();
+        WindowInsetsCompat windowInsets = ViewCompat.getRootWindowInsets(activity.getWindow().getDecorView());
 
-            if (windowInsets != null) {
-                float density = res.getDisplayMetrics().density;
-
-                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
-                    Insets insets = windowInsets.getInsets(
-                        WindowInsetsCompat.Type.systemBars() | WindowInsetsCompat.Type.displayCutout() | WindowInsets.Type.navigationBars()
-                    );
-                    leftInset = insets.left / density;
-                    rightInset = insets.right / density;
-                    topInset = insets.top / density;
-                    bottomInset = insets.bottom / density;
-                } else if (Build.VERSION.SDK_INT == Build.VERSION_CODES.Q) {
-                    final DisplayCutout cutout = windowInsets.getDisplayCutout();
-
-                    leftInset = cutout != null ? cutout.getSafeInsetLeft() : 0;
-                    rightInset = cutout != null ? cutout.getSafeInsetRight() : 0;
-                    topInset = cutout != null ? cutout.getSafeInsetTop() : 0;
-                    bottomInset = cutout != null ? cutout.getSafeInsetBottom() : 0;
-
-                    Insets insets = windowInsets.getSystemWindowInsets();
-                    leftInset = Math.max(leftInset, insets.left) / density;
-                    rightInset = Math.max(rightInset, insets.right) / density;
-                    topInset = Math.max(topInset, insets.top) / density;
-                    bottomInset = Math.max(bottomInset, insets.bottom) / density;
-                } else {
-                    leftInset = windowInsets.getSystemWindowInsetLeft() / density;
-                    rightInset = windowInsets.getSystemWindowInsetRight() / density;
-                    topInset = windowInsets.getSystemWindowInsetTop() / density;
-                    bottomInset = windowInsets.getSystemWindowInsetBottom() / density;
-                }
-            }
+        if (windowInsets != null) {
+            float density = res.getDisplayMetrics().density;
+            Insets insets = windowInsets.getInsets(WindowInsetsCompat.Type.systemBars() | WindowInsetsCompat.Type.displayCutout());
+            leftInset = insets.left / density;
+            rightInset = insets.right / density;
+            topInset = insets.top / density;
+            bottomInset = insets.bottom / density;
         }
 
         JSObject ret = new JSObject();
